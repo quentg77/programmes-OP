4e3614fe9044ed7cee585f94fde44fbb7e50e237
quentg77 <q.guichaoua@gmail.com>
2018-11-03 17:48:25 +0100

    update test

--- reacteur/lib/affichage.lua
+++ reacteur/lib/affichage.lua
@@ -50,7 +50,7 @@ local function _bloc(bloc)
 	-- construit le contenue du bloc
 	if bloc.content ~= nil then
 		for i,line in ipairs(bloc.content) do
-			if type(line) = "string" then
+			if type(line) == "string" then
 				gpu.set(bloc.x + mar + bor + pad, bloc.y + mar + bor + pad + (i-1), line)
 			else
 				error(string.format("la ligne n° %i n'est pas reconu par le bloc comme un type valide"), i)
